0c42eb76903c3ded779101fda46d744bd531c1b3
tangentstorm <michal.wallace@gmail.com>
2012-11-22 19:10:10 -0600

    added basic constructors and printers

--- lysp.pas
+++ lysp.pas
@@ -5,7 +5,33 @@ uses gc, ln;
 {-- lisp data structures --}
 
 type
+  _tag = ( t_nil, t_int, t_str, t_sym, t_cel,
+           t_sub, t_exp, t_fsub, t_fexp, t_psub );
+  pstr = ^string;
+  cell = class;
+  celldata = record
+  case tag : _tag of
+    t_nil : ( );
+    t_int : ( int : integer );
+    t_str : ( str : pstr );
+    t_sym : ( sym : pstr );
+    t_cel : ( cel : cell );
+    t_sub : ( sub : cell );
+    t_exp : ( exp, env : cell );
+    //  foreign functions
+    // t_fsub : ( fsub : cell );
+    // t_fexp : ( fexp : cell );
+    // t_psu	 : ( psub : cell );
+  end;
   cell = class
+    tag : _tag; data : celldata;
+    constructor make_nil;
+    constructor make_int( int : integer );
+    constructor make_str( str : string );
+    constructor make_sym( sym : string );
+    constructor make_cel( cel : cell );
+    constructor make_sub( sub : cell );
+    constructor make_exp( exp, env : cell );
   end;
   chargen = procedure ( var ch : char );
   reader = function ( next : chargen ) : cell;
@@ -14,6 +40,36 @@ var
   readers : array[ char ] of reader;
   main_prompt : string = '--> ';
   more_prompt : string = '... ';
+
+{-- cell types --}
+
+  constructor cell.make_nil;
+  begin self.tag := t_nil;
+  end;
+
+  constructor cell.make_int( int : integer );
+  begin tag := t_int; data.int := int
+  end;
+
+  constructor cell.make_str( str : string );
+  begin tag := t_str; data.str := @str
+  end;
+
+  constructor cell.make_sym( sym : string );
+  begin tag := t_sym; data.sym := @sym
+  end;
+
+  constructor cell.make_cel( cel : cell );
+  begin tag := t_cel; data.cel := cel
+  end;
+
+  constructor cell.make_sub( sub : cell );
+  begin tag := t_sub; data.sub := sub
+  end;
+
+  constructor cell.make_exp( exp, env : cell );
+  begin tag := t_exp; data.exp := exp; data.env := env
+  end;
 
 {-- reader --}
 
@@ -85,14 +141,17 @@ var
     use( @read_semi,   ';' );
   end;
 
-  function parse( line : string; var expr : cell ): boolean;
-  begin result := true;
+  function parse( line : string; var expr : cell ) : boolean;
+  begin
+    expr := cell.make_nil;
+    result := true;
   end;
 
-{-- garbage collection callbacks --}
+{-- evaluator --}
 
-  procedure eval( expr, result :  cell );
+  function eval( expr : cell ) : cell;
   begin
+    result := expr;
   end;
 
 {-- garbage collection callbacks --}
@@ -111,6 +170,22 @@ var
   begin
   end;
 
+  function print( value : cell ) : cell;
+  begin
+    with value do begin
+      case tag of
+        t_nil : ;
+        t_int : write( data.int );
+        t_str : write( '"', data.str^, '"' );
+        t_sym : write( data.sym^ );
+        t_cel : write( '<cel>' );
+        t_sub : write( '<sub>' );
+        t_exp : write( '<exp>' );
+      end;
+    end;
+    result := value;
+  end;
+
 {-- read-eval-print loop --}
 
   procedure completion( const buf : string; var comps : ln.Completions );
@@ -118,18 +193,20 @@ var
   end;
 
   procedure repl;
-    var line, prompt : string; expr, result : cell;
+    var line, prompt : string; expr, value : cell;
   begin
     prompt := main_prompt;
     ln.on_complete := @completion;
     while ln.prompt( prompt, line ) do begin
       writeln;
       prompt := main_prompt;
-      if parse( line, expr ) then
-        begin
-	  eval( expr, result );
+      if parse( line, expr ) then begin
+	value := eval( expr );
+	if value.tag <> t_nil then begin
+	  print( value );
+	  writeln;
 	end
-      else prompt := more_prompt
+      end else prompt := more_prompt
     end;
     writeln;
   end;
